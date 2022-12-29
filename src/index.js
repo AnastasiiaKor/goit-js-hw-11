e0b0df79654f52152680fbba8529d8911533cf8d
Anastasiya Kormilitsina <anastasiya.kormilitsina@gmail.com>
2022-12-30 03:13:10 +0500

    fixes bugs

--- src/index.js
+++ src/index.js
@@ -8,30 +8,41 @@ import { addBackToTop } from 'vanilla-back-to-top';
 
 const form = document.querySelector('#search-form');
 const galleryEl = document.querySelector('.gallery');
-
+const guard = document.querySelector('.js-guard');
 const {
   elements: [input, button],
 } = form;
 let isFirstFetch = true;
 let page = 1;
+const options = {
+  root: null,
+  rootMargin: '300px',
+  threshold: 1.0,
+};
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: `alt`,
+  captionDelay: 250,
+});
+let totalHits = 0;
 
-const infiniteObserver = new IntersectionObserver(([entry], observer) => {
-  if (entry.isIntersecting) {
-    observer.unobserve(entry.target);
+const infiniteObserver = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
     isFirstFetch = false;
-    if (page === 13) {
-      Notify.failure(
-        "We're sorry, but you've reached the end of search results."
-      );
-
-      return;
+    if (entry.isIntersecting) {
+      console.log(galleryEl.children.length);
+      if (galleryEl.children.length === totalHits) {
+        Notify.failure(
+          "We're sorry, but you've reached the end of search results."
+        );
+        infiniteObserver.unobserve(entry.target);
+        return;
+      }
+      loadPics(input.value, (page += 1));
     }
-    loadPics(input.value, (page += 1));
-  }
-}, {});
+  });
+}, options);
 form.addEventListener('submit', onFormSubmit);
 form.addEventListener('input', onFormInput);
-galleryEl.addEventListener('click', onClick);
 
 addBackToTop({
   backgroundColor: '#c72169',
@@ -41,41 +52,17 @@ addBackToTop({
 
 Notify.init(settings);
 
-function onClick(event) {
-  event.preventDefault();
-
-  let gallery = new SimpleLightbox('.gallery a', {
-    captionsData: `alt`,
-    captionDelay: 250,
-  });
-}
-
 function onFormSubmit(event) {
   event.preventDefault();
+
   if (input.value) {
     galleryEl.innerHTML = '';
     page = 1;
     loadPics(input.value, page);
-
     button.disabled = true;
   } else {
     Notify.failure('Plese, fill the search field!');
   }
-}
-function getData(data) {
-  if (!data.hits.length) {
-    Notify.failure(
-      'Sorry, there are no images matching your search query. Please try again.'
-    );
-    return;
-  }
-  if (isFirstFetch) {
-    Notify.success(`Hooray! We found ${data.totalHits} images.`);
-  }
-
-  const { hits } = data;
-  const markup = renderCard({ hits });
-  galleryEl.insertAdjacentHTML('beforeend', markup);
 }
 
 function onFormInput(event) {
@@ -84,23 +71,38 @@ function onFormInput(event) {
   }
   button.disabled = false;
 }
-function loadPics(value) {
-  fetchData(value, page)
-    .then(response => response.data)
-    .then(data => {
-      getData(data);
-      const { height: cardHeight } = document
-        .querySelector('.gallery')
-        .firstElementChild.getBoundingClientRect();
-
-      window.scrollBy({
-        top: cardHeight * 0.3,
-        behavior: 'smooth',
-      });
-      const lastCard = document.querySelector('.photo-card:last-child');
-      if (lastCard) {
-        infiniteObserver.observe(lastCard);
-      }
-    })
-    .catch(error => console.log(error));
+async function loadPics(value) {
+  try {
+    const response = await fetchData(value, page);
+    const { data } = response;
+    totalHits = data.totalHits;
+    renderGallery(data.hits, totalHits);
+  } catch (error) {
+    console.log(error);
+  }
+}
+function renderGallery(hits, totalHits) {
+  if (!hits.length) {
+    Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    return;
+  }
+  if (isFirstFetch) {
+    Notify.success(`Hooray! We found ${totalHits} images.`);
+  }
+  const markup = renderCard({ hits });
+  galleryEl.insertAdjacentHTML('beforeend', markup);
+  infiniteObserver.observe(guard);
+  gallery.refresh();
+  smoothScroll();
+}
+function smoothScroll() {
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+  window.scrollBy({
+    top: cardHeight * 0.2,
+    behavior: 'smooth',
+  });
 }
